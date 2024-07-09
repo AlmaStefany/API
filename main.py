1a66dbcf5a2858e3d5cfe9d4d2e9f028b03cbfbe
AlmaStefany <vitoria.higino6@gmail.com>
2024-07-09 18:58:19 -0300

    Link 2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 """Instalção inicial:
 abrir o terminal e dar o seguinte comando: pip install fastapi
 apos a instalação, digitar no terminal: pip install uvicorn."""
+import logging
+
+import utils.scraping
 
 '''Testar se a APi esta no ar: 
 vc precisa abrir o terminal e digitar: uvicorn nome do arquivo (no meu caso aqui é
@@ -12,8 +15,10 @@ from fastapi import FastAPI, HTTPException, Query
 from utils.scraping import scrap_website
 from utils.find_links import find_links_at_level_one, find_links_at_level_two
 
+logging.basicConfig(level=logging.INFO)
+
 app = FastAPI()
-@app.get('/links')
+@app.get('/links/')
 async def get_website_links():
     #faz o scraping do site especificado.
     website_content = scrap_website('http://vitibrasil.cnpuv.embrapa.br/download/')
@@ -32,5 +37,46 @@ async def get_website_links():
 
     #retorna o link contraido com resposta
     return {'links': links}
+
+@app.get('/links2/')
+async def get_website_links2(link: str = Query(..., description='URL do link para acessar')):
+    """
+       Rota GET para obter links do segundo nível de um website.
+
+       Args:
+           link (str): A URL do link para acessar.
+
+       Returns:
+           dict: Um dicionário contendo uma lista de links encontrados.
+
+       Raises:
+           HTTPException: Se o parâmetro 'link' estiver ausente, ou se nenhum conteúdo ou link for encontrado.
+       """
+    # Registra o link recebido
+    logging.info(f'recebendo link: {link}')
+
+    if not link:
+        raise HTTPException(status_code=400, detail='link é obrigatorio')
+
+    #faz o scraping do site especificado pelo link
+    website_content = utils.scraping.scrap_website(link)
+
+    if not website_content:
+        # lança uma exceção
+        raise HTTPException(status_code=404, detail='nenhum conteúdo encontrado')
+
+    # extrai os links do conteúdo da pagina no primeiro nivel
+    links = find_links_at_level_two(website_content)
+
+    if not links:
+        raise HTTPException(status_code=404, detail='nenhum link encontrado')
+
+    return {'links': links}
+
+
+
+
+
+
 
 
