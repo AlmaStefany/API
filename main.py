5e3e27b0aea3522af34d3bb606d8d46c92a54aee
AlmaStefany <vitoria.higino6@gmail.com>
2024-07-09 17:41:13 -0300

    Link 1

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ apos a instalação, digitar no terminal: pip install uvicorn."""
 vc precisa abrir o terminal e digitar: uvicorn nome do arquivo (no meu caso aqui é
 main) e o nome do aplicativo (no caso aqui é app) e por ultimo digitar --reload então fica: uvicorn main:app --reload'''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from utils.scraping import scrap_website
+from utils.find_links import find_links_at_level_one, find_links_at_level_two
 
 app = FastAPI()
 @app.get('/links')
 async def get_website_links():
-    website_content = scrap_website('http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_01')
-    return website_content
+    #faz o scraping do site especificado.
+    website_content = scrap_website('https://iseb3.com.br/respostas-em-planilhas')
+
+    #verifica se há conteúdo no site
+    if not website_content:
+        #lança uma exceção
+        raise HTTPException(status_code=404, detail='nenhum conteúdo encontrado')
+
+    #extrai os links do conteúdo da pagina no primeiro nivel
+    links = find_links_at_level_one(website_content)
+
+    #verifica se tem link extraidos
+    if not links:
+        raise HTTPException(status_code=404, detail='nenhum conteúdo encontrado')
+
+    #retorna o link contraido com resposta
+    return {'links': links}
